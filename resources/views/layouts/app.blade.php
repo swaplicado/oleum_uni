c94f709718421d376d5c3ac0f0f9be5461faa5d7
Edwin Carmona <eedwincarmona@gmail.com>
2021-10-07 14:00:06 -0500

    link favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/uvaeth_aeth.png') }}">
     <title>{{ env('APP_NAME', false) }}</title>
 
     <!-- Scripts -->
