e6c293900a61ac74fc08efe8c8e15b14879aad5a
Edwin Carmona <eedwincarmona@gmail.com>
2022-06-27 12:48:03 -0500

    Home videos carousel

--- resources/views/carousel.blade.php
+++ resources/views/carousel.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
     <div class="col-12">
-        <div id="homeCarousel" class="carousel slide" data-interval="500000">
+        <div id="homeCarousel" class="carousel slide" data-bs-ride="carousel" data-interval="500000">
             <div class="carousel-indicators">
                 @for ($i = 0; $i < count($lCarousel); $i++) <button type="button" data-bs-target="#homeCarousel"
                     data-bs-slide-to="{{ $i }}" class="{{ $i == 0 ? 'active' : '' }}"
@@ -18,7 +18,7 @@
                                     <p style="color: {{ $lCarousel[$i]->text_color }}">{{ $lCarousel[$i]->text }}</p>
                                 </div>
                             @else
-                                <video controls="" name="media">
+                                <video controls="" autoplay name="media" class="carousel-video">
                                     <source id="idSource" src="{{ $lCarousel[$i]->path }}" type="video/mp4">
                                 </video>
                             @endif
