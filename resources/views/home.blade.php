5a1a5b3b6ffc1cbbc6589422f3dde064257a9ea5
Adrián Avilés <adrian.aviles@swaplicado.com.mx>
2022-06-29 09:22:13 -0500

    imagen de fondo en cards cuadrantes y cursos en vista home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,28 +3,32 @@
 @section('content')
 @section('content_title', $title)
 @section('css_section')
-<link rel="stylesheet" type="text/css" href="{{ asset('myapp/css/percent_circle.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('myapp/css/percent_circle.css') }}" />
 @endsection
 <div class="row">
     <div class="row">
-            <div class="col-md-3">
-                <div class="card text-dark bg-light" style="width: 18rem;">
-                    <div class="card-header bg-light" style="text-align: center">
-                        <img src="{{ asset(\Auth::user()->profile_picture) }}" class="card-img-top" alt="..." style="height: 10rem; width: 10rem;">
-                    </div>
-                    <div class="card-body">
-                      <div style="text-align: center;">
-                        <a href="{{ route('profile') }}"><h5 class="card-title">{{ \Auth::user()->username }}</h5></a>
-                      </div>
-                        <p><b>Num. empleado: </b>{{ \Auth::user()->num_employee }}</p>
-                        <p><b>Puesto: </b>{{ \Auth::user()->job->job }}</p>
-                        <a class="btn btn-primary" href="{{route('kardex.index')}}" target="_blank">Mi avance</a>
-                    </div>
+        <div class="col-md-3">
+            <div class="card text-dark bg-light" style="width: 18rem;">
+                <div class="card-header bg-light" style="text-align: center">
+                    <img src="{{ asset(\Auth::user()->profile_picture) }}" class="card-img-top" alt="..."
+                        style="height: 10rem; width: 10rem;">
                 </div>
-            </div>
-            <div class="col-md-9">
-                @include('carousel')
-            </div>
+                <div class="card-body">
+                    <div style="text-align: center;">
+                        <a href="{{ route('profile') }}">
+                            <h5 class="card-title">{{ \Auth::user()->username }}</h5>
+                        </a>
+                    </div>
+                    <p><b>Num. empleado: </b>{{ \Auth::user()->num_employee }}</p>
+                    <p><b>Puesto: </b>{{ \Auth::user()->job->job }}</p>
+                    <a class="btn btn-primary" href="{{ route('kardex.index') }}" target="_blank">Mi avance</a>
+                </div>
+              </div>
+              <br>
+        </div>
+        <div class="col-md-9">
+            @include('carousel')
+        </div>
     </div>
     <hr>
     <div class="row">
@@ -39,21 +43,28 @@
             <h5>No tienes competencias asignadas</h5>
         @endif
         @foreach ($lAssignments as $ka)
-            <div class="col-lg-3 col-md-6 col-12">
+            <div class="col-lg-3 col-md-6 col-12 ">
                 <a href="{{ route('uni.modules.index', [$ka->id_assignment, $ka->id_knowledge_area]) }}">
-                    <div class="card border-primary text-dark bg-light mb-3" style="max-width: 18rem;">
-                        <div class="card-header text-header-blue" style="height: 5rem;">
-                            {{ $ka->knowledge_area }}
+                    <div class="card border-primary text-dark bg-light mb-3" style="max-width: 18rem; height: 25rem;">
+                        <div class="card-header"
+                            style="height: 8rem; background-image: url('{{ asset('img/job.jpg') }}'); background-size: 18rem 8rem;">
+                            <progress class="progress" value="{{ $ka->completed_percent }}" max="100"
+                                style="margin-left: 30%; margin-top: 3%;"></progress>
                         </div>
-                        <div class="card-body" style="height: 16rem;">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">
+                                <h5 class="card-title text-primary">{{ $ka->knowledge_area }}</h5>
+                            </li>
+                        </ul>
+                        <div class="card-body">
                             <p class="card-text">{{ $ka->description }}</p>
-                            <progress class="progress" value="{{$ka->completed_percent}}" max="100" style="margin-left: 25%;"></progress>
                         </div>
-                        <div class="card-footer text-muted">
-                            <a style="width: 95%"
-                                href="{{ route('uni.modules.index', [$ka->id_assignment, $ka->id_knowledge_area]) }}"
-                                class="btn btn-info" type="button">Iniciar cursos</a>
-                            <br>
+                        <div class="card-footer">
+                            <small class="text-muted">
+                                <button style="width: 95%"
+                                    href="{{ route('uni.modules.index', [$ka->id_assignment, $ka->id_knowledge_area]) }}"
+                                    class="btn btn-info" type="button">Iniciar cursos</button>
+                            </small>
                         </div>
                     </div>
                 </a>
@@ -74,16 +85,17 @@
             <div class="col-lg-3 col-md-6 col-12">
                 <a href="{{ route('uni.courses.course', [$course->id_course, $course->assignment_id]) }}">
                     <div class="card border-success">
-                        <img src="{{ asset('img/curso-capacitacion-snb.png') }}" class="card-img-top" alt="">
+                        <div class="card-header"
+                            style="height: 8rem; background-image: url('{{ asset('img/curso-capacitacion-snb.png') }}'); background-size: 18rem 8rem;">
+                            <progress class="progress" value="{{ $course->completed_percent }}" max="100"
+                                style="margin-left: 25%;"></progress>
+                        </div>
                         <div class="card-body">
                             <h5 class="card-title">{{ $course->course }}</h5>
                             <p class="card-text">{{ $course->description }}</p>
                             <p class="card-text"><small
                                     class="text-muted">{{ 'Comenzado ' . \Carbon\Carbon::parse($course->dtt_take)->diffForHumans() }}</small>
                             </p>
-                        </div>
-                        <div class="card-footer text-muted">
-                          <progress class="progress" value="{{ $course->completed_percent }}" max="100" style="margin-left: 25%;"></progress>
                         </div>
                     </div>
                 </a>
